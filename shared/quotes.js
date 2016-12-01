08786c3857ebd75db58835ce9f5d84ce8ce5cadf
Björn Gustafsson <bjorn.gustafsson@tretton37.com>
2016-12-01 12:23:57 +0100

    Add external data

---
+++ shared/quotes.js
@@ -0,0 +1,10 @@
+export default [
+	{q: "I have made software great again", w: "JoelSannerstedt"},
+	{q: "it wouldn't be frontend if it felt right", w: "AndersRingqvist"},
+	{q: "And so it shall be known. Martin Sewart is the official name of @martin.stewart.", w: "MikaelBrassman"},
+	{q: "I’m prehistoric… (╥﹏╥)", w: "MikaelBrassman"},
+	{q: "yes, it is, children ARE delicious", w: "JustinNel"},
+	{q: "@christiantallhamn:  are you allergic to poop!?", w: "StellanRunander"},
+	{q: "Why did I even think this was going to gain a sensible way to ask a question?", w: "VictoriaHolmqvist"},
+	{q: "I do, sometimes I spy on your kids", w: "NejcPalir"}
+]
